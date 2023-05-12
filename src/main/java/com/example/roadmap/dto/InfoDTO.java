45fe04f95db573f176871f85084dd955c1a6bc07
박재민 <pparkjae0405@naver.com>
2023-05-12 20:52:30 +0900

    feat: InfoDTO 추가

---
+++ src/main/java/com/example/roadmap/dto/InfoDTO.java
@@ -0,0 +1,59 @@
+package com.example.roadmap.dto;
+
+import com.example.roadmap.domain.Info;
+import com.example.roadmap.domain.Roadmap;
+import lombok.*;
+
+public class InfoDTO {
+    /**
+     * 댓글의 등록과 수정을 처리할 요청(Request) 클래스
+     */
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Request {
+        private Long infoId;
+        private String date;
+        private String title;
+        private String content;
+        private Roadmap roadmap;
+
+        /* Dto -> Entity */
+        public Info toEntity() {
+            Info infos = Info.builder()
+                    .infoId(infoId)
+                    .date(date)
+                    .title(title)
+                    .content(content)
+                    .roadmap(roadmap)
+                    .build();
+
+            return infos;
+        }
+    }
+
+    /**
+     * 댓글 정보를 리턴할 응답(Response) 클래스
+     * Entity 클래스를 생성자 파라미터로 받아 데이터를 Dto로 변환하여 응답
+     * 별도의 전달 객체를 활용해 연관관계를 맺은 엔티티간의 무한참조를 방지
+     */
+    @Getter
+    @RequiredArgsConstructor
+    public static class Response {
+        private Long infoId;
+        private String date;
+        private String title;
+        private String content;
+        private Long roadmapId;
+
+        /* Entity -> Dto*/
+        public Response(Info info) {
+            this.infoId = info.getInfoId();
+            this.date = info.getDate();
+            this.title = info.getTitle();
+            this.content = info.getContent();
+            this.roadmapId = info.getRoadmap().getRoadmapId();
+        }
+    }
+}
