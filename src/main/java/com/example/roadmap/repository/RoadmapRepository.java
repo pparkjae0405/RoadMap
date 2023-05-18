90109d274f34acabd714b1bc50445ed065c5e7f2
박재민 <pparkjae0405@naver.com>
2023-05-18 21:36:45 +0900

    feat: RoadmapRepository 추가
    - 최초 조회 여부에 따른 기능 추가

--- src/main/java/com/example/roadmap/repository/RoadmapRepository.java
+++ src/main/java/com/example/roadmap/repository/RoadmapRepository.java
@@ -1,9 +1,16 @@
 package com.example.roadmap.repository;
 
 import com.example.roadmap.domain.Roadmap;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoadmapRepository extends JpaRepository<Roadmap, Long> {
+    // 최초로 조회한 경우(cursor == null)
+    Page<Roadmap> findAllByOrderByRoadmapIdDesc(PageRequest pageRequest);
+
+    // 최초가 아닌 경우(유효한 cursor)
+    Page<Roadmap> findByRoadmapIdLessThanOrderByRoadmapIdDesc(Long cursor, PageRequest pageRequest);
 }
