31b5749cb6c3ca3b86442f061698032b818d639f
박재민 <pparkjae0405@naver.com>
2023-05-23 19:49:42 +0900

    feat: Controller 공통 주소 묶기

--- src/main/java/com/example/roadmap/controller/TagController.java
+++ src/main/java/com/example/roadmap/controller/TagController.java
@@ -10,23 +10,22 @@ import java.util.List;
 
 @RestController // Restuful 웹서비스의 컨트롤러, Json 형태로 객체 데이터를 반환
 @RequiredArgsConstructor // final 혹은 @NotNull이 붙은 필드의 생성자를 자동으로 만들어준다
+@RequestMapping(value = "/tour") // 공통 주소 묶기
 public class TagController {
     private final TagService tagService;
 
     /**
-     * 태그 쓰기 ( POST /tour/{roadmapId}/tag )
-     *
+     * 태그 쓰기 ( POST /{roadmapId}/tag )
      */
-    @PostMapping("/tour/{roadmapId}/tag")
+    @PostMapping("/{roadmapId}/tag")
     public ResponseEntity save(@PathVariable Long roadmapId, @RequestBody List<TagDTO.Request> dto) {
         return ResponseEntity.ok(tagService.save(roadmapId, dto));
     }
 
     /**
-     * 태그 삭제 ( DELETE /tour/{roadmapId}/tag )
-     *
+     * 태그 삭제 ( DELETE /{roadmapId}/tag )
      */
-    @DeleteMapping("/tour/{roadmapId}/tag")
+    @DeleteMapping("/{roadmapId}/tag")
     public ResponseEntity delete(@PathVariable Long roadmapId) {
         tagService.delete(roadmapId);
         return ResponseEntity.ok(roadmapId);
