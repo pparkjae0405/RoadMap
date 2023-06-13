f1368b56fc2faab8163aef046951393dbf3848bf
박재민 <pparkjae0405@naver.com>
2023-06-13 18:24:26 +0900

    fix: User 테이블 수정
    - major에 null 허용
    - 회원번호, 닉네임, 이메일, 직무 외 Column 삭제
    - Roadmap의 FK 주석 추가

--- src/main/java/com/example/roadmap/domain/User.java
+++ src/main/java/com/example/roadmap/domain/User.java
@@ -1,46 +1,58 @@
 package com.example.roadmap.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * 회원정보를 위한 User 테이블
+ */
+@Builder
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 생성
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 만듦
+@Getter
 @Entity
 public class User {
-
+    /**
+     * 회원 번호(PK)
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "userId")
-    private long userId;
+    private Long userId;
 
+    /**
+     * 닉네임, 이메일, 직무
+     */
     @Column(name = "nickName", nullable = false, unique = true)
     private String nickName;
 
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String major;
 
-    @Column(name="serialNumber")
-    private String serialNumber;
+    /**
+     * User는 Roadmap, Comment와 일대다 연관관계를 가짐
+     */
+    // mappedBy = "user" : FK는 다 쪽에서 관리
+    // cascade = cascadeType.ALL : 상위 엔터티에서 하위 엔터티로 모든 작업을 전파
+    // orphanRemoval = true : 부모 엔티티가 삭제되면 자식 엔티티도 삭제
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Roadmap> roadmaps;
 
-    @Column(name = "accessToken")
-    private String accessToken;
-
-    @Column(name="accesstokenExpire")
-    private String accesstokenExpire;
-
-    @Column(name = "refreshToken")
-    private String refreshToken;
-
-    @Column(name = "refreshTokenExpire")
-    private String refreshTokenExpire;
-
-    // comment 외래키 설정
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
 
-    // roadMap 외래키 설정
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Roadmap> roadmaps;
+    /**
+     * 직무 수정
+     */
+    public void update(String major) {
+        this.major = major;
+    }
 }
