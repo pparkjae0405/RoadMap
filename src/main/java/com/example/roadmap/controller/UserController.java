7023fb350940f8758c4475f82ff6457d84a4ca38
박재민 <pparkjae0405@naver.com>
2023-06-13 18:26:28 +0900

    fix: 회원 API 수정

--- src/main/java/com/example/roadmap/controller/UserController.java
+++ src/main/java/com/example/roadmap/controller/UserController.java
@@ -6,26 +6,31 @@ import com.example.roadmap.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequestMapping(value = "/user")
-@RequiredArgsConstructor
+@RestController // Restuful 웹서비스의 컨트롤러, Json 형태로 객체 데이터를 반환
+@RequiredArgsConstructor // final 혹은 @NotNull이 붙은 필드의 생성자를 자동으로 만들어준다
 public class UserController {
-
     private final UserService userService;
 
-    // 내 정보 조회 Api
-    @GetMapping(value = "/{userId}/info")
+    /**
+     * 내 정보 조회
+     */
+    @GetMapping(value = "/user/{userId}/info")
     public String viewMyInfo(@PathVariable long userId) {
         return "";
     }
-    // 내 정보 수정
-    @PutMapping(value = "/{userId}/info")
+
+    /**
+     * 직무 수정
+     */
+    @PutMapping(value = "/user/{userId}/info")
     public String editMyInfo(@PathVariable long userId) {
         return "";
     }
 
-    // 활동 내역 조회
-    @GetMapping(value = "/{userId}/activity")
+    /**
+     * 활동내역 조회
+     */
+    @GetMapping(value = "/user/{userId}/activity")
     public String viewActivityHistory(@PathVariable long userId) {
         return "";
     }
