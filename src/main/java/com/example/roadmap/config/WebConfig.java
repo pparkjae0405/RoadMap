e55f5c654137f95518a71d7a8b8c90e648767aa0
박재민 <pparkjae0405@naver.com>
2023-06-19 19:31:47 +0900

    fix: 포트번호 수정 및 주석 추가

--- src/main/java/com/example/roadmap/config/WebConfig.java
+++ src/main/java/com/example/roadmap/config/WebConfig.java
@@ -6,9 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /* 프론트 서버와의 CORS 이슈를 해결하기 위한 메소드*/
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080");
+                .allowedOrigins("http://localhost:5173");
     }
 }
