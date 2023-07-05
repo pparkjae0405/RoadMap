f4bcda01737b522f7260548423c844bc930ee396
박재민 <pparkjae0405@naver.com>
2023-07-05 20:36:28 +0900

    feat: 회원정보 조회 구현

--- src/main/java/com/example/roadmap/service/UserService.java
+++ src/main/java/com/example/roadmap/service/UserService.java
@@ -1,11 +1,15 @@
 package com.example.roadmap.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.roadmap.config.exception.CEmailLoginFailedException;
+import com.example.roadmap.dto.UserDTO;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import com.example.roadmap.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor // final 혹은 @NotNull이 붙은 필드의 생성자를 자동으로 만들어준다
@@ -13,5 +17,22 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    
+    /**
+     * 회원정보 조회
+     */
+    @Transactional
+    public UserDTO.Response readMyInfo() {
+        // 현재 사용자의 인증 정보를 가져온다.
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // 사용자의 이메일을 가져온 뒤
+        org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) principal;
+        String email = ((User) principal).getUsername();
+
+        // 회원정보를 받아와 UserDTO.Response 형태로 매핑하여 설정
+        UserDTO.Response userResponse = new UserDTO.Response(userRepository.findByEmail(email)
+                .orElseThrow(CEmailLoginFailedException::new));
+
+        return userResponse;
+    }
 }
