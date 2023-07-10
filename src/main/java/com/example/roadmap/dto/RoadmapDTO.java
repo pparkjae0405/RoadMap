2fcc8816a9dc2181f25d0133e9b072949a079b6a
박재민 <pparkjae0405@naver.com>
2023-07-10 17:41:09 +0900

    fix: 활동내역 조회 API 리턴값 수정
    - 글 작성자의 닉네임도 리턴하도록 수정

--- src/main/java/com/example/roadmap/dto/RoadmapDTO.java
+++ src/main/java/com/example/roadmap/dto/RoadmapDTO.java
@@ -195,6 +195,7 @@ public class RoadmapDTO {
     @RequiredArgsConstructor
     public static class ActivityResponse {
         private Long roadmapId;
+        private String nickName;
         private String date;
         private int view;
         private String title;
@@ -202,6 +203,7 @@ public class RoadmapDTO {
 
         public ActivityResponse(Roadmap roadmap){
             this.roadmapId = roadmap.getRoadmapId();
+            this.nickName = roadmap.getUser().getNickName();
             this.date = roadmap.getDate();
             this.view = roadmap.getView();
             this.title = roadmap.getTitle();
